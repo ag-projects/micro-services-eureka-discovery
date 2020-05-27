e0d7345b0baa45754d7cf62ec4318abcea1a5b31
Armen G <gharibi.armen@yahoo.com>
2020-05-27 10:44:37 -0700

    Added authenticationFilter to user-service, added method level pre-post authorizaition

--- user-service/src/main/java/com/agharibi/controllers/UserController.java
+++ user-service/src/main/java/com/agharibi/controllers/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +49,8 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(mappedUser);
     }
 
+    //@PreAuthorize("principal == #userId")
+    @PostAuthorize("principal == returnObject.body.userId")
     @GetMapping(value = "{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserResponseModel> getUser(@PathVariable("userId") String userId) {
         UserDTO userDto = userService.getUserByUserId(userId);
--- user-service/src/main/java/com/agharibi/security/WebSecurity.java
+++ user-service/src/main/java/com/agharibi/security/WebSecurity.java
@@ -2,16 +2,17 @@ package com.agharibi.security;
 
 import com.agharibi.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     private Environment environment;
@@ -34,9 +35,13 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.headers().frameOptions().disable();
-        http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
+        http.authorizeRequests()
+            .antMatchers(HttpMethod.POST, "/users").hasIpAddress(environment.getProperty("gateway.ip"))
+            .antMatchers("/h2-console/**").permitAll()
+            .anyRequest().authenticated()
             .and()
-            .addFilter(getAuthenticationFilter());
+            .addFilter(getAuthenticationFilter())
+            .addFilter(new AuthorizationFilter(authenticationManager(), environment));
     }
 
     private AuthenticationFilter getAuthenticationFilter() throws Exception {
